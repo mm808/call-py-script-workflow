18d2c8bed46d093fcc4a6a6033d6d8ec6d690a3a
mm808 <matt.mangione@gmail.com>
2025-01-20 14:47:46 -0500

    update script code to prep for going public2

--- py_code/echo_items.py
+++ py_code/echo_items.py
@@ -3,7 +3,7 @@ import datetime
 def echo_stuff():
     now = datetime.datetime.now()
     formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Current date and time is {formatted_date_time})
+    print(f"Current date and time is {formatted_date_time}")
 
 Current date and time: 2025-01-20 19:42:16
 
